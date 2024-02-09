633e01b1aaa8fa0fd51b460f0ef033c245f7560f
Lanceiz <atuls.90601@gmail.com>
2024-02-10 01:10:44 +0530

    link-hover-effect-created

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -7,7 +7,7 @@ const NavLink = ({ link }) => {
   const pathName = usePathname();
 
   return (
-    <Link className={`rounded p-1 ${pathName === link.url && "bg-red-500 text-white"}`} href={link.url}>
+    <Link className={`rounded p-1 ${pathName === link.url && "bg-red-500 text-white"} relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`} href={link.url}>
       {link.title}
     </Link>
   );
