971fe649934b14a323f3bbfddf4e5b6faadbed8c
Lanceiz <atuls.90601@gmail.com>
2024-02-12 02:27:23 +0530

    resume can be downloaded now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -57,7 +57,7 @@ const Homepage = () => {
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <Link href="/portfolio">
+            <Link href="https://drive.google.com/file/d/1icgdGkg8sIm8MeTuxZd-E_Rhv42cYXWx/view?usp=drive_link">
             <button className="relative h-12 w-40 overflow-hidden border border-purple-500 text-purple-500 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-purple-500 before:duration-300 before:ease-out hover:text-white hover:shadow-purple-500 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
               <span className="relative z-10">Download Resume</span>
             </button>
