038be83cea47bc6898c9c82471dfaeb91f848864
Atul Singh <111691858+Lanceiz@users.noreply.github.com>
2025-08-21 23:20:21 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import TransitionProvider from "@/components/transitionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Atul-Codes",
+  title: "Lance-Codes",
   description: "The best animated portfolio page",
 };
 
